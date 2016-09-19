13830af96768676ef2fcdb4fa48a6fe30cd12d12
Matt Martell <mattmartell07@gmail.com>
2016-09-19 11:26:07 -0400

    TestingDelegate

---
+++ TestingDelegate/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestingDelegate
+{
+
+    /// <summary>
+    /// A class to define a person
+    /// </summary>
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public enum AgeGroup
+    {
+        Child,
+        Adult,
+        Senior
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>
+            {
+                new Person() {Age = 22, Name = "Bob"},
+                new Person() {Age = 52, Name = "Matt"},
+                new Person() {Age = 21, Name = "Bill"},
+                new Person() {Age = 65, Name = "Jared"},
+                new Person() {Age = 115, Name = "Trish"},
+                new Person() {Age = 5, Name = "Susan"},
+                new Person() {Age = 2, Name = "Kate"},
+                new Person() {Age = 0, Name = "Ken"},
+                new Person() {Age = 18, Name = "Jon"},
+                new Person() {Age = 75, Name = "Sean"},
+                new Person() {Age = 13, Name = "Scott"}
+            };
+
+            DisplayPeople(AgeGroup.Child, people, AgeDescending);
+            DisplayPeople(AgeGroup.Adult, people, AgeDescending);
+            DisplayPeople(AgeGroup.Senior, people, AgeDescending);
+
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        public delegate List<Person> PeopleAgeFilter(List<Person> people);
+
+        public delegate List<Person> PeopleOrderFilter(List<Person> people);
+
+        public static void DisplayPeople(AgeGroup ag, List<Person> people, PeopleOrderFilter f)
+        {
+            var filteredPeople = f(GetAgeFilter(ag)(people));
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Displaying " + Enum.GetName(typeof(AgeGroup), ag));
+            foreach (var person in filteredPeople) Console.WriteLine("Person {0} is {1} years old.", person.Name, person.Age);
+        }
+
+        public static List<Person> AgeDescending(List<Person> people)
+        {
+            return people.OrderBy(x => x.Age).ToList();
+        }
+
+        public static List<Person> Children(List<Person> people)
+        {
+            return people.FindAll(p => p.Age < 18);
+        }
+
+        public static List<Person> Adult(List<Person> people)
+        {
+            return people.FindAll(p => p.Age >= 18 && p.Age < 65);
+        }
+
+        public static List<Person> Senior(List<Person> people)
+        {
+            return people.FindAll(p => p.Age > 65);
+        }
+
+        public static PeopleAgeFilter GetAgeFilter(AgeGroup ag)
+        {
+            switch (ag)
+            {
+                    case AgeGroup.Child: return Children;
+                    case AgeGroup.Adult: return Adult;
+                    case AgeGroup.Senior: return Senior;
+                default: throw new ArgumentException("Unrecognized AgeGroup.");
+            }
+        }
+    }
+}
---
+++ TestingDelegate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestingDelegate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingDelegate")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fe3dd92-b650-4632-8b22-6d2d746e6771")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
